8012799877eaead7b3d2d27d01df1911e85fe0f4
Dang Phuoc Trieu <trieudang77hoaihuong@gmail.com>
2023-11-12 15:48:22 +0700

    clean code

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
-import postRoute from './routes/Post.js';
+import productService from './services/Product_Service.js';
 
 dotenv.config();
+
+const app = express();
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, () => {
     console.log('Connected to MongoDB');
 });
 
-const app = express();
-app.use(express.json());
 
-
-// Routes
-app.use('/api/post', postRoute);
+// Services
+app.use('/api/product-service', productService);
 
 // Start server
 app.listen(9000, () => {
-    console.log('Product API started on PORT 9000');
+    console.log('Product Service API started on PORT 9000');
 });
 
